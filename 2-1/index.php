0aa4993a0461f3811efe82c072c4d702f70de1dd
t-nagatomo <toshikibackn@gmail.com>
2020-06-25 12:39:19 +0900

    Hello World
    IT用語について調べた。（課題提出）

---
+++ 2-1/index.php
@@ -0,0 +1,32 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+1-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+
+    <?php
+     echo "1.開発環境・検証環境・本番環境"."<br>";
+     echo "・開発環境…※機器やソフトウェア、システムを開発するために必要な機材やソフトウェアなどの組み合わせ。また、それらを組み合わせて構成された、開発者の作業環境"."<br>";
+     echo "・検査環境…装置やソフトウェア、システムの開発において、正しく動作するか検証する試験（テスト）のために用意された機材やソフトウェアなどの動作環境"."<br>";
+     echo "・本番環境…システムが製品として実際に稼動している環境"."<br><br>";
+     
+     
+     echo "2.基本設計書・詳細設計書"."<br>";
+     echo "・基本設計書…作るシステムの概要とか基本的なことをまとめた資料"."<br>";
+     echo "・詳細設計書…作るシステムの具体的な処理とかが書いてある資料"."<br><br>";
+
+     echo "3.Ajax"."<br>";
+     echo "・JavaScriptとXMLを使って非同期にサーバとの間の通信を行うこと。"."<br>";
+     
+
+?>
+<!-- <img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"> -->
+</html>
