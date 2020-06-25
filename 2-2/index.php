80485bacb59794d24d4ed515fe3bf39f78bf9a70
t-nagatomo <toshikibackn@gmail.com>
2020-06-25 13:04:19 +0900

    変数とデータ型
    変数とデータ型課題作成（提出）
    IT用語調べる

---
+++ 2-2/index.php
@@ -0,0 +1,40 @@
+<?php
+$message = "Hello World!";
+var_dump($message);
+//echo $message;
+echo '<br>';
+
+
+$x = 8;
+$y = 5.55;
+echo $x;
+var_dump($x);
+echo '<br>';
+echo $y;
+var_dump($y);
+echo '<br>';
+
+$flag = true;
+var_dump($flag);
+echo '<br>';
+
+$emp = null;
+var_dump($emp);
+echo '<br>'; 
+
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';  //文字列の長さは 半角英数字を基準にしているため、文字列の長さが多い。
+var_dump($ja_words);
+
+echo '<br>';  
+echo '<br>';    
+
+
+     echo "1.API"."<br>";
+     echo "・ソフトウェアやアプリケーションなどの一部を外部に向けて公開することにより、第三者が開発したソフトウェアと機能を共有できるようにしてくれるもの。"."<br><br>";
+
+     echo "2.SVN（Subversion）"."<br>";
+     echo "・オープンソースの集中型バージョン管理システム。正式名称はApache Subversion。ファイルの変更履歴を管理してくれるシステム。"."<br>";
+?>
