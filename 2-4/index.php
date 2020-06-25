91842598b82b3db1446d87fead73029fae1859c4
t-nagatomo <toshikibackn@gmail.com>
2020-06-25 14:07:43 +0900

    演算子
    演算子　課題提出
    IT用語調べ

---
+++ 2-4/index.php
@@ -0,0 +1,81 @@
+<?php 
+
+
+$x=5;
+
+echo "hello" . "world" ."<br>";
+
+$hello = "hello";
+$world = "world";
+
+echo $hello . $world;
+
+echo '<br>';
+echo '<br>';
+
+echo 1 + 1;
+echo '<br>';
+echo 10 - 1;
+echo '<br>';
+echo 2 * 2;
+echo '<br>';
+echo 10 / 5;
+echo '<br>';
+echo 5 % 3;
+
+echo '<br>';
+echo '<br>';
+
+
+// $x = $x + 1
+$x++; 
+echo $x;
+echo '<br>';
+
+// $x = $x - 1
+$x--;
+echo $x;
+echo '<br>';
+
+$x = 0;
+ // $x = $x + 5;
+$x += 5;
+echo $x;
+echo '<br>';
+
+ // $x = $x * 5;
+$x *= 5;
+echo $x;
+echo '<br>';
+
+
+
+//計算試し
+$y =3;
+echo $y * ($y + 2);
+
+$y =2;
+
+$y ++; //  $y = $y + 1
+echo '<br>';
+
+$y = $y * 5 +( $y / 3);  
+echo $y;
+
+//ここまで
+echo '<br>';
+echo '<br>';
+
+
+echo "1. エビデンス"."<br>";
+echo "・何かの証拠となるもの。"."<br><br>";
+
+echo "2. バッチ"."<br>";
+echo "・日本語では「一束、一群、一団」といった意味があります。
+つまりバッチ処理とは“一定量の(あるいは一定期間の)データを集め、一括処理するための処理方法。"."<br><br>";
+
+echo "3. MVCモデル"."<br>";
+echo "・Model View Controllerの略称。機能ごとにプログラムの中身（処理）を分けて記述することで、プログラムを整理しやすくする考え方。分け方は3種類。"."<br><br>";
+
+
+
