904635118bff98b710d6e7119c89e4328ec2bc69
t-nagatomo <toshikibackn@gmail.com>
2020-06-25 23:50:56 +0900

    フォームに入力した内容を取得する
    スペース修正。

--- 2-11/result.php
+++ 2-11/result.php
@@ -6,5 +6,5 @@ $password = $_POST['password'];
 ?>
 <p>私の名前は<?php echo $name ;?></p>
 <p>私のメールアドレスは<?php echo $mail ;?></p>
-<p>私のパスワードは<?php echo $password  ;?></p>
+<p>私のパスワードは<?php echo $password ;?></p>
 
