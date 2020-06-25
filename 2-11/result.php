1c6550ed208d005343b8f102e6b1e06bbc749483
t-nagatomo <toshikibackn@gmail.com>
2020-06-25 19:14:56 +0900

    フォームに入力した内容を取得する
    フォームに入力した内容を取得する　課題作成
    IT用語調べ

---
+++ 2-11/result.php
@@ -0,0 +1,10 @@
+<?php 
+
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+?>
+<p>私の名前は<?php echo $name ;?></p>
+<p>私のメールアドレスは<?php echo $mail ; ?></p>
+<p>私のパスワードは<?php echo $password  ;?></p>
+
